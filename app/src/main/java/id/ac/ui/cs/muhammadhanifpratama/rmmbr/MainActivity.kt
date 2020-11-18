e4b54d8a6354995cfe06116349edd22363082038
Hanif Pratama <mhp.hanif5@gmail.com>
2020-11-18 23:25:50 +0700

    add main fragment

--- app/src/main/java/id/ac/ui/cs/muhammadhanifpratama/rmmbr/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/muhammadhanifpratama/rmmbr/MainActivity.kt
@@ -2,10 +2,20 @@ package id.ac.ui.cs.muhammadhanifpratama.rmmbr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.ui.cs.muhammadhanifpratama.rmmbr.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val mainFragment: MainFragment = MainFragment.newInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, mainFragment)
+                .commitNow()
+        }
     }
 }
