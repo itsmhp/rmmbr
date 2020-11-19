3944915ff57e03c9061d697cf35ee01d7e22319e
Hanif Pratama <mhp.hanif5@gmail.com>
2020-11-19 16:38:06 +0700

    add navigation from MainFragment to NotesFragment and VoiceNotesFragment

--- app/src/main/java/id/ac/ui/cs/muhammadhanifpratama/rmmbr/ui/main/NotesFragment.kt
+++ app/src/main/java/id/ac/ui/cs/muhammadhanifpratama/rmmbr/ui/main/NotesFragment.kt
@@ -2,20 +2,18 @@ package id.ac.ui.cs.muhammadhanifpratama.rmmbr.ui.main
 
 //import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import id.ac.ui.cs.muhammadhanifpratama.rmmbr.R
-import kotlinx.android.synthetic.main.fragment_main.*
-import kotlinx.android.synthetic.main.main_activity.view.*
+import kotlinx.android.synthetic.main.fragment_notes.*
 
-
-class MainFragment : Fragment() {
+class NotesFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance() = NotesFragment()
     }
 
     //private lateinit var viewModel: BlankViewModel
@@ -24,27 +22,16 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_notes, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val ft: FragmentTransaction = fragmentManager!!.beginTransaction()
         //viewModel = ViewModelProviders.of(this).get(BlankViewModel::class.java)
         // TODO: Use the ViewModel
-
-        NotesBtn.setOnClickListener {
-            ft.replace(R.id.container, NotesFragment(), "NewFragmentTag")
-            ft.addToBackStack(null)
-            ft.commit()
+        toolbar.setNavigationOnClickListener {
+            this.activity!!.onBackPressed()
         }
-
-        VNBtn.setOnClickListener {
-            ft.replace(R.id.container, VoiceNotesFragment(), "NewFragmentTag")
-            ft.addToBackStack(null)
-            ft.commit()
-        }
-
     }
 
 }
